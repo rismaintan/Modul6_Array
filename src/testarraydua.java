48054092a98d79a098434bdc5f03966e6616e97d
rismaintanw <rismaintanw@rismaiw>
2017-09-26 10:51:55 -0700

    Modul6_Array

---
+++ src/testarraydua.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rismaintanw
+ */
+public class testarraydua {
+
+    int nis[][]={{210651},{210652},{210653}};
+    String nama[][]={{"Risma Intan"},{"Intan Wulandari"},{"Risma Wulandari"}};
+    
+    public void tampil(){
+        System.out.println("Indentitas Siswa Angkatan 25");
+    }
+    public void NamaNis(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<1;j++){
+                System.out.println(nama[i][j]+" : "+nis[i][j]);
+            }
+        }
+    } 
+    public static void main(String[]args){
+        testarraydua siswa = new testarraydua();
+        siswa.tampil();
+        siswa.NamaNis();
+    }
+}
